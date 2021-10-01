389c8e858920d39993eea656264bd51aa4351a17
steppanovva <steppanovva@gmail.com>
2021-10-01 19:26:01 +0300

    init

---
+++ Shops/Models/Consumer.cs
@@ -0,0 +1,16 @@
+﻿namespace Shops.Models
+{
+    public class Consumer
+    {
+        private string _name;
+        private double _money;
+        public Consumer(string name, int money)
+        {
+            _name = name;
+            _money = money;
+        }
+
+        public string Name { get => _name; }
+        public double Money { get => _money; set => _money = value; }
+    }
+}
---
+++ Shops/Models/ShopManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+namespace Shops.Models
+{
+    public class ShopManager
+    {
+        private static int _shopId = 1;
+        private static List<Product> _products;
+        private List<Shop> _shops;
+        public ShopManager()
+        {
+            _shops = new List<Shop>();
+            _products = new List<Product>();
+        }
+
+        public static List<Product> RegisteredProducts { get => _products; }
+        public Shop Create(string name, int money)
+        {
+            var shop = new Shop(name, _shopId++, money);
+            _shops.Add(shop);
+            return shop;
+        }
+
+        public Product RegisterProduct(string name, double primeCost)
+        {
+            var product = new Product(name, primeCost, 1);
+            _products.Add(product);
+            return product;
+        }
+
+        public Shop FindProfitableShop(Dictionary<string, int> products)
+        {
+            Shop minPriceShop = null;
+            double minPrice = -1.0;
+            foreach (var shop in _shops)
+            {
+                var availableProducts = shop.AvailableProducts(products);
+                double total = Shop.TotalCost(availableProducts);
+                if (!(total < minPrice) && minPrice != -1.0) continue;
+                minPrice = total;
+                minPriceShop = shop;
+            }
+
+            return minPriceShop;
+        }
+    }
+}
